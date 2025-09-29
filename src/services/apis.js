cf47978061e6798a2030c34acde6fe282dbb4159
Divyanshguptaj <luckys510039@gmail.com>
2025-09-29 21:35:03 +0530

    updated backend route

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:4000/api/v1";
-// const BASE_URL = process.env.REACT_APP_BACKEND_URL || "https://academix-w1rw.onrender.com/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "https://academix-w1rw.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
